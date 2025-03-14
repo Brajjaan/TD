1920b66383244e96d6f5588a032eb5c1b178b360
Brajjaan <bryanvouriot@hotmail.com>
2025-03-14 17:44:04 +0100

    Editor & Waypoint system
    Created scripts for pathing for the AI.
    Created an editor script that allows the inspector to interact with editor arrays.

---
+++ Assets/Scripts/Editor/WaypointEditor.cs
@@ -0,0 +1,39 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Waypoint))]
+public class WaypointEditor : Editor
+{
+    Waypoint Waypoint => target as Waypoint;
+
+    private void OnSceneGUI()
+    {
+        Handles.color = Color.white;
+        for (int i = 0; i < Waypoint.Points.Length; i++)
+        {
+            EditorGUI.BeginChangeCheck();
+            
+            Vector3 currentWaypointPoint = Waypoint.CurrentPosition + Waypoint.Points[i];
+            var fmh_19_17_638775705727580103 = Quaternion.identity; Vector3 newWaypointPoint = Handles.FreeMoveHandle(
+                currentWaypointPoint, 0.7f,
+                new Vector3(0.3f, 0.3f, 0.3f), Handles.SphereHandleCap);
+
+            // Create text
+            GUIStyle textStyle = new GUIStyle();
+            textStyle.fontStyle = FontStyle.Bold;
+            textStyle.fontSize = 16;
+            textStyle.normal.textColor = Color.white;
+            Vector3 textAlignment = Vector3.down * 0.35f + Vector3.right * 0.35f;
+            Handles.Label(Waypoint.CurrentPosition + Waypoint.Points[i] + textAlignment,
+                $"{i + 1}", textStyle);
+
+            EditorGUI.EndChangeCheck();
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(target, "Free Move Handle");
+                Waypoint.Points[i] = newWaypointPoint - Waypoint.CurrentPosition;
+            }
+        }
+    }
+}
